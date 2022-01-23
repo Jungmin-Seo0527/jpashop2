cfebcd2f7eaabdcac039161293d7ba932bbaa79c
Jungmin-Seo0527 <soato1405@naver.com>
2022-01-23 17:55:45 +0900

    2-5. 엔티티 설계시 주의점

--- src/main/java/jpabook/jpashop2/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop2/domain/OrderItem.java
@@ -11,6 +11,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 @Getter @Setter
 public class OrderItem {
@@ -19,11 +21,11 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "item_id")
     private Item item;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 
