573ec137dbfe49b7b6121775453d27e13e640d38
Jungmin-Seo0527 <soato1405@naver.com>
2022-01-23 17:31:16 +0900

    2-4. 엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop2/domain/Address.java
+++ src/main/java/jpabook/jpashop2/domain/Address.java
@@ -1,11 +1,17 @@
 package jpabook.jpashop2.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
+import static lombok.AccessLevel.PROTECTED;
+
 @Embeddable
 @Getter
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
 public class Address {
 
     private String city;
--- src/main/java/jpabook/jpashop2/domain/item/Item.java
+++ src/main/java/jpabook/jpashop2/domain/item/Item.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop2.domain.item;
 
+import jpabook.jpashop2.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +11,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -24,4 +28,7 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 }
